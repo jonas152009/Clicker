de1064c71d20bf1273432f2a7d544b96047bf81c
Jonas Spaniol <jonas.spaniol@europapark.de>
2025-07-15 14:41:57 +0200

    addad requsts for user

---
+++ clicker-backend/src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+
+@Injectable()
+export class UsersService {
+  users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    const user: User ={name: createUserDto.name, age: createUserDto.age};
+    this.users.push(user);
+    return createUserDto;
+  }
+  
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users[id];
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const userIndexOutOfRange = id > this.users.length && id < 0;
+    if (userIndexOutOfRange) {
+      return;
+    }
+
+    this.users[id].name = updateUserDto.name ?? this.users[id].name;
+    this.users[id].age = updateUserDto.age ?? this.users[id].age;
+  }
+
+  remove(id: number) {
+    this.users.splice(id);
+  }
+}
